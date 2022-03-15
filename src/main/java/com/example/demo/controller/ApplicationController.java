f21d7b7466195f606e2eae8347e0ce884ac5e912
gbarse <gorbarseghyan01@gmail.com>
2022-03-15 16:17:16 +0400

    add functionality to add new application

--- src/main/java/com/example/demo/controller/ApplicationController.java
+++ src/main/java/com/example/demo/controller/ApplicationController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Application;
 import com.example.demo.service.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicationController {
     @Autowired
     private ApplicationService ApplicationService;
+
     // display list of applications
     @GetMapping("/")
     public String viewHomePage(Model model){
@@ -17,4 +21,17 @@ public class ApplicationController {
         return "index";
     }
 
+    @GetMapping("/showNewApplicationForm")
+    public String showNewApplicationForm(Model model) {
+        Application application = new Application();
+        model.addAttribute("application", application);
+        return "new_application";
+    }
+
+    @PostMapping("/saveApplication")
+    public String saveApplication(@ModelAttribute("Application")Application application){
+        //save application to the database;
+        ApplicationService.saveApplication(application);
+        return "redirect:/";
+    }
 }
